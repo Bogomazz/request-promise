206f9bea514e0f0327b1685412f8b9c729dd7f8c
Anton Bogomaz <bogomaz97@gmail.com>
2017-03-26 11:19:54 +0300

    Second commit.

---
+++ index.js
@@ -0,0 +1,24 @@
+let request             = require('request');
+
+let requestPromise = {};
+
+requestPromise.send = (options)=>{
+    return new Promise((resolve, reject) =>{
+        request(options, (err, resp, body) =>{
+            if (!err){
+                let response = {
+                    status: resp.statusCode,
+                    body,
+                    headers: resp.headers
+                }
+                resolve(response);
+                return;
+            }
+            else {
+                reject(err);
+            }
+        })
+    });
+}
+
+module.exports = requestPromise;
